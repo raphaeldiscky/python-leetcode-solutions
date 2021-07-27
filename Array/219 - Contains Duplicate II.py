a7c328f99748da35e3a3136adf5d58cf0f0b5a19
Raphael Discky <zundria.putra@gmail.com>
2021-07-27 21:54:00 +0700

    Add solution 219

---
+++ Array/219 - Contains Duplicate II.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic = {}
+        for i, v in enumerate(nums):
+            print("i:", i, "v:", v)
+            if v in dic and i - dic[v] <= k:  # if v is in same as key in dic
+                return True
+            dic[v] = i
+        return False
+
+
+ob1 = Solution()
+nums = [1, 2, 3, 1]
+k = 3
+print(ob1.containsNearbyDuplicate(nums, k))
