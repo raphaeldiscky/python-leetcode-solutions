8e90e13e3fdf19a3c6628b6e9214c1ab7a6f1831
Raphael Discky <zundria.putra@gmail.com>
2021-07-29 21:51:59 +0700

    Add solution 101

--- Binary Tree/100 - Same Tree.py
+++ Binary Tree/100 - Same Tree.py
@@ -19,6 +19,19 @@ class Solution:
             return False
         return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
 
+    def dfs_stack(self, p, q):
+        stack = [(p, q)]
+        while stack:
+            n1, n2 = stack.pop()
+            if n1 and n2 and n1.val == n2.val:
+                stack.append((n1.right, n2.right))
+                stack.append((n1.left, n2.left))
+            elif not n1 and not n2:
+                continue
+            else:
+                return False
+        return True
+
 
 root1 = TreeNode(1)
 root1.left = TreeNode(2)
@@ -32,3 +45,4 @@ root2.left.right = TreeNode(5)
 
 ob = Solution()
 print(ob.isSameTree(root1, root2))
+print(ob.dfs_stack(root1, root2))
---
+++ Binary Tree/101 - Symmetric Tree.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        '''
+            Time: O(n)
+            Space: O(n)
+        '''
+        if not root:
+            return True
+        stack = []
+        stack.append((root.left, root.right))
+        while stack:
+            l, r = stack.pop()
+            if not l and not r:
+                return True
+            if not l or not r:
+                return False
+            if l.val != r.val:
+                return False
+            stack.append((l.left, r.right))
+            stack.append((l.right, r.left))
+        return True
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(2)
+root.left.right = TreeNode(4)
+root.left.left = TreeNode(3)
+root.right.right = TreeNode(3)
+root.right.left = TreeNode(4)
+
+ob = Solution()
+print(ob.isSymmetric(root))
