feb7127fb445578920264b659f0a43535a65aa8a
Raphael Discky <zundria.putra@gmail.com>
2021-08-04 20:02:08 +0700

    Add solution 145

---
+++ Binary Tree/145 - BTree Postorder Traversal.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # recursive
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, root, res):
+        if root:
+            self.dfs(root.left, res)
+            self.dfs(root.right, res)
+            res.append(root.val)
+
+    # iterative
+    def postorderTraversal2(self, root: TreeNode) -> List[int]:
+        stack, res = [root], []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]
+
+
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.left = TreeNode(3)
+ob = Solution()
+print(ob.postorderTraversal(root))
+print(ob.postorderTraversal2(root))
